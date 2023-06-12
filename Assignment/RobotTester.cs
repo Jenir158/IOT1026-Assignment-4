886cfcb025e7cd77a8b9e688c7abe3e792092d58
Jenir158 <131227098+Jenir158@users.noreply.github.com>
2023-06-12 03:03:16 +0000

    New File

---
+++ Assignment/RobotTester.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace Assignment
+{
+    /// <summary>
+    /// Represents a simple robot tester class.
+    /// </summary>
+    public static class RobotTester
+    {
+        /// <summary>
+        /// Tests the robot by assigning and running commands.
+        /// </summary>
+        public static void TestRobot()
+        {
+            // Create a new instance of the Robot class
+            Robot newRobot = new();
+
+            // Display available commands to the user
+            Console.WriteLine("Give 6 commands to the robot. Possible commands are:");
+            Console.WriteLine("on, off, north, south, east, west");
+
+            int input = 0;
+            do
+            {
+                Console.Write($"Assign command #{input + 1}: ");
+                bool commandAdded = AssignCommand(newRobot, Console.ReadLine());
+                if (commandAdded)
+                {
+                    input++;
+                }
+            } while (input < 6);
+
+            // Run the robot
+            newRobot.Run();
+        }
+
+        /// <summary>
+        /// Assigns a command to the robot based on the user input.
+        /// </summary>
+        /// <param name="robot">The Robot object.</param>
+        /// <param name="command">The user input representing the command.</param>
+        /// <returns>True if the command was added successfully, false otherwise.</returns>
+        private static bool AssignCommand(Robot robot, string? command)
+        {
+            switch (command)
+            {
+                case "on":
+                    robot.LoadCommand(new OnCommand());
+                    return true;
+                case "off":
+                    robot.LoadCommand(new OffCommand());
+                    return true;
+                case "north":
+                    robot.LoadCommand(new NorthCommand());
+                    return true;
+                case "south":
+                    robot.LoadCommand(new SouthCommand());
+                    return true;
+                case "east":
+                    robot.LoadCommand(new EastCommand());
+                    return true;
+                case "west":
+                    robot.LoadCommand(new WestCommand());
+                    return true;
+                default:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Invalid command - Try again");
+                    Console.ResetColor();
+                    return false;
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
